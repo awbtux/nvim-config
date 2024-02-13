02950a737d4657f84af79872cd59891975f37f86
awbtux <andy@antareslinux.org>
2024-02-13 15:43:09 -0800

    main init.lua

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- icons
+vim.g.icons = require "user.icons"
+
+-- options
+require "user.options"
+
+-- vim/plugin keybinds
+require "user.keymaps"
+
+-- autocmds for different events
+require "user.autocmds"
